2ec94b77beb592b07f210927ffe445dfe3bae35a
jiagguojun <602735865@qq.com>
2020-12-26 22:47:51 +0800

    新增模块

---
+++ TestGitProcuct/Me/View/MeViewHead.h
@@ -0,0 +1,17 @@
+//
+//  MeViewHead.h
+//  TestGitProcuct
+//
+//  Created by 姜国军 on 2020/12/26.
+//  Copyright © 2020 Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MeViewHead : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TestGitProcuct/Me/View/MeViewHead.m
@@ -0,0 +1,21 @@
+//
+//  MeViewHead.m
+//  TestGitProcuct
+//
+//  Created by 姜国军 on 2020/12/26.
+//  Copyright © 2020 Jiang. All rights reserved.
+//
+
+#import "MeViewHead.h"
+
+@implementation MeViewHead
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
